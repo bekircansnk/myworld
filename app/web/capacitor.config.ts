fe22d337b6d0d84981da9947f9554ff6fe43ef43
bekircansnk <sagnakbekircan@gmail.com>
2026-04-29 12:27:08 +0300

    chore: create backup Android app variant

--- app/web/capacitor.config.ts
+++ app/web/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.bekirsagank.myworld',
-  appName: 'My World',
+  appId: 'com.bekirsagank.myworld.backup',
+  appName: 'My World Backup',
   webDir: 'out'
 };
 
